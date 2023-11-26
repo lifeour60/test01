9439abafc34dfb7ef432da9616695c25dedff5d4
lifeour60 <152089974+lifeour60@users.noreply.github.com>
2023-11-27 01:37:27 +0800

    python auto run
    python auto run

---
+++ catch.py
@@ -0,0 +1,101 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Line Notify 的 Access Token
+LINE_NOTIFY_ACCESS_TOKEN = 'S6prDMx4jetGxvYrBe4fsBoSz2HVYuQCPUHLwOsKXIr'
+
+# 目标网页的URL
+#tkttube url = 'https://tktube.com/latest-updates/'
+# njav new url = 'https://njav.tv/zh/recent-update?page=1'
+url = 'https://njav.tv/zh/tags/fc2'
+ur2 = 'https://njav.tv/zh/new-release?page=1'
+
+# 发送HTTP请求并获取网页内容
+response = requests.get(url)
+
+# Line Notify 的发送函数
+def send_line_notify(token, message):
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    headers = {'Authorization': f'Bearer {token}'}
+    data = {'message': message}
+    requests.post(line_notify_api, headers=headers, data=data)
+    
+# 检查请求是否成功
+if response.status_code == 200:
+    # 使用Beautiful Soup解析网页内容
+    soup = BeautifulSoup(response.text, 'html.parser')   
+    list_videos_container = soup.find('div', class_='row box-item-list gutter-20')
+    
+    if list_videos_container:
+            # 查找所有包含视频信息的<div>标签
+            video_items = list_videos_container.find_all('div', class_='col-6 col-sm-4 col-lg-3')
+
+            # 存储标题和链接的列表
+            titles_and_links = []
+
+            # 遍历每个视频信息块，提取标题和链接
+            for video_item in video_items:
+                # 提取标题
+                title = video_item.find('div', class_='detail').find('a').text.strip()
+                # 提取链接
+                link = video_item.find('div', class_='detail').find('a')['href']
+
+                full_href = f"https://njav.tv/zh/{link}"
+
+
+                # 将标题和链接添加到列表
+                titles_and_links.append((title, full_href))
+
+            # 将标题和链接发送到 Line Notify
+            for title, link in titles_and_links:
+                message = f"Title: {title}\n網址: {link}"
+                send_line_notify(LINE_NOTIFY_ACCESS_TOKEN, message)
+
+           # line字數限制
+           # messages = "\n".join([f"Title: {title}\n網址: {link}" for title, link in titles_and_links])
+           # send_line_notify(LINE_NOTIFY_ACCESS_TOKEN, messages)
+    else:
+        print('Not find list_videos_container')
+
+
+    #----------tktube-------------
+    '''
+    # 查找包含视频信息的<div>标签
+    list_videos_container = soup.find('div', class_='list-videos')
+    if list_videos_container:
+        video_container = list_videos_container.find('div', {'id': 'list_videos_latest_videos_list_items'})
+
+        if video_container:
+            # 查找所有包含视频信息的<div>标签
+            video_items = video_container.find_all('div', class_='item')
+
+            # 存储标题和链接的列表
+            titles_and_links = []
+
+            # 遍历每个视频信息块，提取标题和链接
+            for video_item in video_items:
+                # 提取标题
+                title = video_item.find('strong', class_='title').text.strip()
+
+                # 提取链接
+                link = video_item.find('a')['href']
+
+                # 将标题和链接添加到列表
+                titles_and_links.append((title, link))
+
+            # 将标题和链接发送到 Line Notify
+            for title, link in titles_and_links:
+                message = f"Title: {title}\n網址: {link}"
+                send_line_notify(LINE_NOTIFY_ACCESS_TOKEN, message)
+
+           # line字數限制
+           # messages = "\n".join([f"Title: {title}\n網址: {link}" for title, link in titles_and_links])
+           # send_line_notify(LINE_NOTIFY_ACCESS_TOKEN, messages)
+
+        else:
+            print('Not find video_container')
+    else:
+        print('Not find list_videos_container')
+    '''
+else:
+    print('request fail')
